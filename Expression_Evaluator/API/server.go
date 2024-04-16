b5f766bbdd60f2992a1c9280fe63b1ff4004cd47
Nikolay Ditchev <58337919+NikolayDitchev@users.noreply.github.com>
2024-04-16 21:46:57 +0300

    Update server.go
    added handler for GET request (landing page)

--- Expression_Evaluator/API/server.go
+++ Expression_Evaluator/API/server.go
@@ -16,7 +16,8 @@ func (s *APIserver) Run() error {
 
 	router := http.NewServeMux()
 	router.HandleFunc("POST /evaluate", middlewareChain(evaluateExpression))
-
+	router.HandleFunc("GET /evaluate", getHtmlFile)
+	
 	server := http.Server{
 		Addr:    s.addr,
 		Handler: router,
